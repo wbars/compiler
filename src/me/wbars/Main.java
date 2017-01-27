8a7c8e78a9a0cccbc1fca1cff1ae4f6f62773abf
wannabe <smelov.kirill@gmail.com>
2017-01-27 20:26:58 +1000

    Simple scanner implementation with sample pascal tokens

--- src/me/wbars/Main.java
+++ src/me/wbars/Main.java
@@ -1,6 +1,8 @@
 package me.wbars;
 
 import me.wbars.scanner.Scanner;
+import me.wbars.scanner.io.FileGrammarReader;
+import me.wbars.scanner.io.GrammarReader;
 import me.wbars.scanner.io.ScannerFilePersister;
 import me.wbars.scanner.models.Token;
 import me.wbars.scanner.models.TransitionTable;
@@ -13,9 +15,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        GrammarReader stringGrammarReader = new FileGrammarReader("grammar.txt");
-//        TransitionTable table = stringGrammarReader.readTable();
-//        ScannerFilePersister.writeToFile(table, "table");
+        GrammarReader stringGrammarReader = new FileGrammarReader("tokens.txt");
+        TransitionTable table = stringGrammarReader.readTable();
+        ScannerFilePersister.writeToFile(table, "table");
         TransitionTable table1 = ScannerFilePersister.fromFile("table");
         List<Token> scan = Scanner.scan(getFileContents("file1.txt"), table1);
         scan.forEach(System.out::println);
